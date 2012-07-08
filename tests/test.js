64322d22884aba27a5536466e052a8ab9f782b7e
Dave Eddy <dave@daveeddy.com>
2012-07-08 16:53:09 -0700

    Fixing tests

--- tests/test.js
+++ tests/test.js
@@ -4,23 +4,22 @@
  */
 
 var assert = require('assert'),
-    autocast = require('../'),
-    str_int = '5',
-    str_float = '5.6',
-    str_fakenumber = '5.6.7',
-    str_false = 'false',
-    str_true = 'true',
-    str_null = 'null',
-    str_undefined = 'undefined',
-    str_normalstr = 'Just a string';
+    autocast = require('../');
 
-console.log('Testing Numbers');
-assert.strictEqual(autocast(str_int), 5);
-assert.strictEqual(autocast(str_float), 5.6);
-assert.strictEqual(autocast(str_fakenumber), '5.6.7');
+console.log('Testing Numbers...');
+assert.strictEqual(autocast('5'), 5);
+assert.strictEqual(autocast('5.6'), 5.6);
+assert.strictEqual(autocast('5.6.7'), '5.6.7');
+console.log('ok');
 
-console.log('Testing common data types');
-assert.strictEqual(autocast(str_false), false);
-assert.strictEqual(autocast(str_true), true);
-assert.strictEqual(autocast(str_null), null);
-assert.strictEqual(autocast(str_undefined), undefined);
+console.log('Testing common data types...');
+assert.strictEqual(autocast('false'), false);
+assert.strictEqual(autocast('true'), true);
+assert.strictEqual(autocast('null'), null);
+assert.strictEqual(autocast('undefined'), undefined);
+console.log('ok');
+
+console.log('Testing strings...');
+assert.strictEqual(autocast('some string'), 'some string');
+assert.strictEqual(autocast('!'), '!');
+console.log('ok');
